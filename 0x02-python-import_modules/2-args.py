b64a4b2fa4459d4b15a52e3b457b2a474b81a797
AlexisCORAN <alexis_ca_@outlook.com>
2021-04-29 23:48:09 +0000

    Task

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,11 +3,11 @@ from sys import argv
 if __name__ == "__main__":
     argc = len(argv) - 1
     if argc == 0:
-        print("0 arguments")
+        print("0 arguments.")
     elif argc == 1:
-        print("1 argument")
+        print("1 argument:")
         print("1: {:s}".format(argv[1]))
     else:
-        print("{:d} arguments".format(argc))
+        print("{:d} arguments:".format(argc))
         for i in range(1, argc + 1):
             print("{:d}: {:s}".format(i, argv[i]))
