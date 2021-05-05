8965070c3f7c60e4300e9ace503098cd9f3790a6
AlexisCORAN <alexis_ca_@outlook.com>
2021-05-05 02:40:25 +0000

    Task

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -3,9 +3,8 @@ def element_at(my_list, idx):
     if idx < 0:
         return (None)
     else:
-        idx += 1
         for count, num in enumerate(my_list):
-            if num == idx:
+            if count == idx:
                 return (num)
         else:
             return (None)
