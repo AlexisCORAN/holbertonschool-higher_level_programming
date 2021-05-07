c8a7bfa572eb46b4cb9f68dd65e61da081747be1
AlexisCORAN <alexis_ca_@outlook.com>
2021-05-07 03:49:49 +0000

    Task

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -3,7 +3,7 @@ def best_score(a_dictionary):
     result = 0
     if a_dictionary:
         for k in a_dictionary:
-            if a_dictionary[key] > result:
+            if a_dictionary[k] > result:
                 result = a_dictionary[k]
                 win = k
         return win
