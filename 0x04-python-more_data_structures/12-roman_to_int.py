ad87f6a4c1c4b7b0bb373a0818c5693e74c6e632
AlexisCORAN <alexis_ca_@outlook.com>
2021-05-08 05:01:11 +0000

    Task

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    roman = {
+        'M':1000,
+        'D':500,
+        'C':100,
+        'L':50,
+        'X':10,
+        'V':5,
+        'I':1
+    }
+    num = 0
+    for i in range(len(roman_string)):
+        if i > 0 and roman[roman_string[i]] > roman[roman_string[i - 1]]:
+            num += roman[roman_string[i]] - 2 * roman[roman_string[i - 1]]
+        else:
+            num += roman[roman_string[i]]
+    return (num)
