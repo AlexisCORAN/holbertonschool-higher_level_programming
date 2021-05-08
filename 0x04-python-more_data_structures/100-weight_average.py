012c656c6e8ba762549bfed5a2e83d23f00443fb
AlexisCORAN <alexis_ca_@outlook.com>
2021-05-08 05:38:58 +0000

    Task

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -2,7 +2,7 @@
 def weight_average(my_list=[]):
     if my_list:
         tot = 0
-        div= 0
+        div = 0
         for x in my_list:
             tot += x[0] * x[1]
             div += x[1]
