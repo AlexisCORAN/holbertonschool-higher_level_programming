75324af8c7e14da0baf8e419e7d004c04c01ac83
AlexisCORAN <alexis_ca_@outlook.com>
2021-05-08 05:15:49 +0000

    Task

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
+
+    if roman_string is None and roman_string != str:
+        return 0
+
     roman = {
         'M': 1000,
         'D': 500,
@@ -9,13 +13,11 @@ def roman_to_int(roman_string):
         'V': 5,
         'I': 1
     }
-    if roman_string is None and roman_string != str:
-        return 0
-    else:
-        num = 0
-        for i in range(len(roman_string)):
-            if i > 0 and roman[roman_string[i]] > roman[roman_string[i - 1]]:
-                num += roman[roman_string[i]] - 2 * roman[roman_string[i - 1]]
-            else:
-                num += roman[roman_string[i]]
-        return (num)
+
+    num = 0
+    for i in range(len(roman_string)):
+        if i > 0 and roman[roman_string[i]] > roman[roman_string[i - 1]]:
+            num += roman[roman_string[i]] - 2 * roman[roman_string[i - 1]]
+        else:
+            num += roman[roman_string[i]]
+    return (num)
