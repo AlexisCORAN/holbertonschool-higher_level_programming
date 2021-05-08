90890190df7ff9d0b09eea18c0a9c95da7fa75bc
AlexisCORAN <alexis_ca_@outlook.com>
2021-05-08 05:06:30 +0000

    Task

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -9,6 +9,8 @@ def roman_to_int(roman_string):
         'V': 5,
         'I': 1
     }
+    if roman_string is None or roman_string != str:
+        return none
     num = 0
     for i in range(len(roman_string)):
         if i > 0 and roman[roman_string[i]] > roman[roman_string[i - 1]]:
