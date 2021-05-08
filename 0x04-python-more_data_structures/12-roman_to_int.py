78520a0f365618d6afa437bbda857c2d495ff83c
AlexisCORAN <alexis_ca_@outlook.com>
2021-05-08 05:04:17 +0000

    Task

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
     roman = {
-        'M':1000,
-        'D':500,
-        'C':100,
-        'L':50,
-        'X':10,
-        'V':5,
-        'I':1
+        'M': 1000,
+        'D': 500,
+        'C': 100,
+        'L': 50,
+        'X': 10,
+        'V': 5,
+        'I': 1
     }
     num = 0
     for i in range(len(roman_string)):
