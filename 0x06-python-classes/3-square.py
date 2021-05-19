d0b1224d12587e84a2fe3132b99a9902cdddcfbc
AlexisCORAN <alexis_ca_@outlook.com>
2021-05-19 03:56:35 +0000

    Task

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -3,14 +3,16 @@
 
 
 class Square:
-     """A class named Square
+    """Represents a square
     Attributes:
-    __size (int): size of a side of the square
+        __size (int): size of a side of the square
     """
     def __init__(self, size=0):
-        """
+        """initializes the square
         Args:
-            size (int): size for  attribute of class instance
+            size (int): size of a side of the square
+        Returns:
+            None
         """
         if type(size) is not int:
             raise TypeError("size must be an integer")
