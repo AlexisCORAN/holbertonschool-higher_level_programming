4805d093656d9515d815978966cb55ae4ec1e780
AlexisCORAN <alexis_ca_@outlook.com>
2021-06-03 00:50:24 +0000

    Task

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""
+This module defines the write_file funciton
+"""
+def write_file(filename="", text=""):
+    """writes a string to a text file (UTF8) and returns the number of characters written"""
+    with open(filename, mode="w", encoding="utf-8") as fh:
+        add_write = fh.write(text)
+    return (add_write)
