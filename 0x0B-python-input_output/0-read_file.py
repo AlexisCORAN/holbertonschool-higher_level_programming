c16aef8890c6067998207a7b424c5bb2ff45936c
AlexisCORAN <alexis_ca_@outlook.com>
2021-06-03 00:41:48 +0000

    Task

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -7,5 +7,4 @@ This module defines the read_file function
 def read_file(filename=""):
     """Reads a text file (UTF8) and prints it to stdout."""
     with open(filename, mode="r", encoding="utf-8") as fh:
-        lines = fh.read()
-        print(lines, end="")
+        print(fh.read(), end="")
