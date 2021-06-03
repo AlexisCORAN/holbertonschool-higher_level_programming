01fb7393a3466d42d0fdff5463f06d319ee9ea14
AlexisCORAN <alexis_ca_@outlook.com>
2021-06-03 03:53:53 +0000

    Task

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+This module defines "Student" class
+"""
+
+
+class Student:
+    """Representation of a student"""
+    def __init__(self, first_name, last_name, age):
+        """Initializes the student"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """retrieves a dictionary representation of a Student instance
+        with specified attributes"""
