f86c5dd6632aabac2acec2219cab37c3f8a991f2
AlexisCORAN <alexis_ca_@outlook.com>
2021-06-04 04:43:14 +0000

    Task

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -15,3 +15,23 @@ class Student:
     def to_json(self, attrs=None):
         """retrieves a dictionary representation of a Student instance
         with specified attributes"""
+        class_dic = self.__dict__
+        sel_dic = dict()
+
+        if type(attrs) is list:
+            for attr in attrs:
+                if type(attr) is not str:
+                    return class_dic
+
+                if attr in class_dic:
+                    sel_dic[attr] = class_dic[attr]
+
+            return sel_dic
+
+        return class_dic
+
+    def reload_from_json(self, json):
+        """replaces all attributes of the Student instance"""
+        for i in json:
+            if i in self.__dict__.keys():
+                self.__dict__[i] = json[i]
