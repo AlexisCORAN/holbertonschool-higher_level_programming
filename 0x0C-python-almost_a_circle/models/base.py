7b73838c1f33916bbbef1e1fcef8df07e2614b9a
AlexisCORAN <alexis_ca_@outlook.com>
2021-06-09 01:13:11 +0000

    Update file

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -2,6 +2,7 @@
 """
 This module defines the Base class
 """
+import json
 
 
 class Base:
@@ -15,3 +16,15 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = self.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """Returns the JSON string representation of list_dictionaries"""
+        if list_dictionaries is None or len(list_dictionaries) is 0:
+            return ("[]")
+        return (json.dumps(list_dictionaries))
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """writes the JSON string representation of list_objs to a file"""
+        filename = cls.__name__ + ".json"
