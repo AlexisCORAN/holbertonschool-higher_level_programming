88a63057a9f8fcc2cb8fc8ff492657189c47bb55
AlexisCORAN <alexis_ca_@outlook.com>
2021-08-02 17:09:29 +0000

    Fixed typo

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,3 +1,3 @@
 #!/usr/bin/node
-const phrases = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+const phrases = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 phrases.forEach(element => console.log(element));
