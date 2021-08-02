10aa63c789e4199e4ab21296881418c9c8933cad
AlexisCORAN <alexis_ca_@outlook.com>
2021-08-02 17:17:36 +0000

    Added tasks

---
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+const myArgs = process.argv.slice(2);
+if (myArgs !== 0) {
+  myArgs.forEach((val) => {
+    console.log(`${val}`);
+  });
+} else {
+  console.log('No argument');
+}
