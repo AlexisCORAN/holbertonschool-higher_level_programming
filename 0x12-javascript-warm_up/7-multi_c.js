1ee38533f13b6162322f4172236640c44a7fbfee
AlexisCORAN <alexis_ca_@outlook.com>
2021-08-02 21:28:01 +0000

    Fixed typo

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
 const myArgs = process.argv.slice(2);
 if (myArgs.length === 0) {
-	console.log('Missing number of occurrences');
+  console.log('Missing number of occurrences');
 } else if (myArgs.length === 1) {
-	let value = myArgs[0];
-	for (let i = 0; i < value; i++) {
-		console.log('C is fun');
-	}
+  const value = myArgs[0];
+  for (let i = 0; i < value; i++) {
+    console.log('C is fun');
+  }
 }
