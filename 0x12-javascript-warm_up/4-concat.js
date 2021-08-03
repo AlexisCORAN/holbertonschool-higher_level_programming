cdbd53162a15a7c15446e63d78c1864c14a8331d
AlexisCORAN <alexis_ca_@outlook.com>
2021-08-03 00:54:17 +0000

    Tasks

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -1,8 +1,6 @@
 #!/usr/bin/node
 var myArgs = process.argv.slice(2);
 
-if (myArgs.length === 2) {
-  let first_value = myArgs[0];
-  let second_value = myArgs[1];
-  console.log('%s is %s', first_value, second_value);
-}
+let first_value = myArgs[0];
+let second_value = myArgs[1];
+console.log('%s is %s', first_value, second_value);
