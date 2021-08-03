0100a588887b31d098659f72e5602be230b304a4
AlexisCORAN <alexis_ca_@outlook.com>
2021-08-03 01:18:33 +0000

    Tasks

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,8 +1,9 @@
 #!/usr/bin/node
 const myArgs = process.argv.slice(2);
-if (myArgs.length === 0 || typeof myArgs[0] === 'string') {
+const value = parseInt(myArgs[0]);
+if (isNaN(value)) {
   console.log('Missing size');
-} else if (myArgs.length === 1) {
+} else {
   const value = myArgs[0];
   for (let i = 0; i < value; i++) {
     console.log('X'.repeat(value));
