dd7f4e360f4e6725f7d76b7b5ba2c45b8b4f9259
AlexisCORAN <alexis_ca_@outlook.com>
2021-08-09 04:58:05 +0000

    Added tasks

--- 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -11,9 +11,8 @@ from sys import argv
 from model_state import Base, State
 
 if __name__ == "__main__":
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(argv[1],
-                                                                    argv[2],
-                                                                    argv[3]))
+    content = 'mysql+mysqldb://{}:{}@localhost/{}'
+    engine = create_engine(content.format(argv[1], argv[2], argv[3]))
     Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
