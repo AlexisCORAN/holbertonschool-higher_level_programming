961e615b925b475a8ac81203d96514eaea802285
AlexisCORAN <alexis_ca_@outlook.com>
2021-08-09 05:01:13 +0000

    Added tasks

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-prints the State object with the name passed as argument from a database
+script that prints the State object with the name
 """
 
 import sqlalchemy
@@ -11,9 +11,9 @@ from model_state import Base, State
 
 if __name__ == "__main__":
     content = 'mysql+mysqldb://{}:{}@localhost/{}'
-    eng = create_engine(content.format(argv[1], argv[2], argv[3]))
-    Base.metadata.create_all(eng)
-    Session = sessionmaker(bind=eng)
+    engine = create_engine(content.format(argv[1], argv[2], argv[3]))
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
     session = Session()
     state = session.query(State).filter_by(name=argv[4]).first()
     if state is not None:
