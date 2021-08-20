8ef25f1d6c5656b0e25ce6ca183e33f100489ce4
AlexisCORAN <alexis_ca_@outlook.com>
2021-08-20 03:15:43 +0000

    Added Advanced tasks

--- 0x10-python-network_0/100-status_code.sh
+++ 0x10-python-network_0/100-status_code.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # This script sends a request to a URL passed as an argument, and displays only the status code of the response.
-curl -sI "$1" | head -n 1 | cut -d " " -f 2
+curl -sI "$1" | head -n 1 | cut -d " " -f 2 | tr -d "\n"
