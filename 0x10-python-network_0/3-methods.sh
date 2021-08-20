797b084c4272a251892f8984b64802df68493f9e
AlexisCORAN <alexis_ca_@outlook.com>
2021-08-20 02:48:15 +0000

    Updated tasks

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # This script takes in a URL and displays all HTTP methods the server will accept.
-curl -siX OPTIONS "$1"  | grep "Allow" | cut -d " " -f 2,3,4
+curl -s -i -X OPTIONS "$1"  | grep "Allow" | cut -d " " -f 2,3,4
