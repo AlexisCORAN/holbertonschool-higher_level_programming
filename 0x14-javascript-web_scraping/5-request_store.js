5922e37122f3f26693f373421954dc772929a15e
AlexisCORAN <alexis_ca_@outlook.com>
2021-09-15 22:08:50 +0000

    Added tasks

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
 const request = require('request');
 const fs = require('fs');
-const filePath = process.argv[3];
-
-request(process.argv[2], function (err, response, body) {
-  fs.writeFile(process.argv[3], body, 'utf8', function (err) {
+const url = process.argv[2];
+const path = process.argv[3];
+request(url, function (_err, response, body) {
+  fs.writeFile(path, body, 'utf-8', function (err) {
     if (err) {
       console.log(err);
     }
