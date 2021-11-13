4146ef431a79c01fa06e518bd7f96233e2c6f48a
AlexisCORAN <alexis_ca_@outlook.com>
2021-11-13 02:16:20 +0000

    Added task

---
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+
+const request = require('request');
+const url = 'https://swapi-api.hbtn.io/api/films/'.concat(process.argv[2]);
+
+request(url, function (err, response, body) {
+  const characters = JSON.parse(body).characters;
+
+  for (let i = 0; i < characters.length; ++i) {
+    request(characters[i], function (cErr, cResponse, cBody) {
+      console.log(JSON.parse(cBody).name);
+    });
+  }
+});
